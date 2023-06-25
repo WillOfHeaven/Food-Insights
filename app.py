6cc509cb9bd555dcf38d3cb12ea6ca6e5d04ac5f
Sambuddha Chatterjee <sambuddha.c30@gmail.com>
2023-06-25 21:19:08 +0530

    Update app.py

--- app.py
+++ app.py
@@ -2,7 +2,6 @@ import base64
 import streamlit as st
 import pandas as pd
 import numpy as np
-import streamlit_authenticator as stauth
 import yaml
 import matplotlib.pyplot as plt
 import matplotlib.pyplot as plt
